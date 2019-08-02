3afc5c38f072a7039fa9067258ec9f787bfb12d6
unknown <yangwenqi@ad61v1.com>
2019-08-02 14:58:46 +0800

    上传课程弹框升级详情接口测试用例

--- run_all.py
+++ run_all.py
@@ -45,6 +45,6 @@ if __name__ == '__main__':
     # main()
 
     logger.setup_logger('debug')
-    path = 'testcases/live/student/popWindow/v1_student_popWindow_courseUpdate.yml'
+    path = 'testcases/live/student/popWindow/v1_student_popWindow_courseUpdate_detail.yml'
     report_path = run(path)
     # del_html()
