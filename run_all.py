8bf61fa5b9bf0aa5c70ead5b5813ab3d9efbb009
git_for_test <git_for_test@git.com>
2019-09-23 17:09:27 +0800

    修改run_all

--- run_all.py
+++ run_all.py
@@ -1,9 +1,5 @@
 import os
-import time
-
 from httprunner.api import HttpRunner
-from httprunner import logger
-from httprunner import locusts
 from common.configemail import send_email
 
 parent_dir = os.path.dirname(os.path.realpath(__file__))
@@ -12,27 +8,35 @@ if not os.path.isdir(report_dir):
     os.makedirs(report_dir)
 
 
-def run(path, gen_report_name=True):
+def run(path, log_level="INFO", log_file=None):
     """
     运行测试用例，返回报告路径
     Args:
         path(str): 测试用例路径，以当前run_all.py的父目录为根目录
-        gen_report_name(bool): 是否以用例名来生成测试报告，等于False时使用时间戳来命名测试报告
     """
-    runner = HttpRunner(failfast=False)
-    report_path = runner.run(path, gen_report_name=gen_report_name)
+    runner = HttpRunner(log_level=log_level, log_file=log_file)
+    report_path = runner.run(path)
     return report_path
 
+def del_html():
+    '''
+    删除html报告
+    '''
+    fl = os.listdir(report_dir)
+    for f in fl:
+        if f.endswith('.html'):
+            f_path = os.path.join(report_dir, f)
+            os.remove(f_path)
 
 def main():
     '''
     执行所用测试用例，并发送报告
     '''
     log_path = os.path.join(report_dir, 'api.log')
-    logger.setup_logger('debug', log_path)
+    # logger.setup_logger('debug', log_path)
 
     path = 'testsuites/'
-    report_path = run(path)
+    report_path = run(path, log_file=log_path)
     send_email(file_path=report_path)
 
 
@@ -40,11 +44,13 @@ if __name__ == '__main__':
     # main()
 
     # logger.setup_logger('info')
-    for t in range(0, 100):
-        for i in range(0, 200):
-            run('testcases/live/liveMonitor/deviceInfo.yml')
-        for i in range(0, 100):
-            run('testcases/live/liveMonitor/ipinfo.yml')
-            run('testcases/live/liveMonitor/dynamicMonitor_android.yml')
-            run('testcases/live/liveMonitor/dynamicMonitor_ios.yml')
-            run('testcases/live/liveMonitor/warn.yml')
+    # for t in range(0, 100):
+    #     for i in range(0, 200):
+    #         run('testcases/live/liveMonitor/deviceInfo.yml')
+    #     for i in range(0, 100):
+    #         run('testcases/live/liveMonitor/ipinfo.yml')
+    #         run('testcases/live/liveMonitor/dynamicMonitor_android.yml')
+    #         run('testcases/live/liveMonitor/dynamicMonitor_ios.yml')
+    #         run('testcases/live/liveMonitor/warn.yml')
+    path = 'testcases/call/login.yml'
+    run(path)
